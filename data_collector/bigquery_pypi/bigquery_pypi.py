f635c9c19ef6d1908be0789883ceb44903a44ddf
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-08 13:38:40 +0200

    Data written and read from file

--- data_collector/bigquery_pypi/bigquery_pypi.py
+++ data_collector/bigquery_pypi/bigquery_pypi.py
@@ -1,25 +1,27 @@
 import os
-import json
-import csv
+import pickle
 from datetime import datetime, timedelta
 from google.cloud import bigquery
 
-def get_most_downloaded_pkgs(limit=None, offset=None):
+date = datetime.now().replace(day=1) - timedelta(days=1)
+table_prefix_date = date.strftime("%Y%m")
+downloads_file = "data_collector/bigquery_pypi/data/downloads_{}.pkl".format(table_prefix_date)
+
+
+def get_most_downloaded_pkgs(limit=100000, offset=None):
+
     downloads_dict = {}
     client = bigquery.Client()
-    date = datetime.now().replace(day=1) - timedelta(days=1)
-    table_prefix_date = date.strftime("%Y%m")
-    current_offset=0
 
     sql_request = "SELECT file.project AS name, COUNT(*) AS download_count, \
     FROM `the-psf.pypi.downloads{}*` \
     GROUP BY name \
-    HAVING download_count>1000 \
+    HAVING download_count>100 \
     ORDER BY download_count DESC".format(table_prefix_date)
     if limit is not None:
-        sql_request += "LIMIT {}".format(limit)
+        sql_request += " LIMIT {}".format(limit)
     if offset is not None:
-        sql_request += "OFFSET {}".format(offset)
+        sql_request += " OFFSET {}".format(offset)
     query_job = client.query(sql_request)
     rows = query_job.result()
 
@@ -27,17 +29,27 @@ def get_most_downloaded_pkgs(limit=None, offset=None):
         downloads_dict[row.name] = row.download_count
     return downloads_dict
 
-def update_or_create_downloads_csv_file():
-    downloads_dict = get_most_downloaded_pkgs()
-    fieldnames=["name", "download_count"]
-    try:
-        with open("most_downloaded.csv", "w") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writeheader()
-            for pkg in downloads_dict:
-                writer.writerow(pkg)
-    except IOError as e:
-        print("I/O ERROR: {}".format(e))
+
+def get_or_create_downloads_dict_from_file():
+
+    if os.path.isfile(downloads_file):
+        f = open(downloads_file)
+        print("Downloads file found: {}".format(downloads_file))
+    else:
+        print("Downloads file NOT found. Requesting downloads from Bigquery")
+        downloads_dict = get_most_downloaded_pkgs()
+        try:
+            with open(downloads_file, 'wb') as f:
+                pickle.dump(downloads_dict, f, pickle.HIGHEST_PROTOCOL)
+                print("Downloads file created: {}".format(downloads_file))
+        except IOError as e:
+            print("I/O ERROR: {}".format(e))
+    with open(downloads_file, 'rb') as f:
+        dict = pickle.load(f)
+        print("Found downloads data for {} packages".format(len(dict)))
+        return
+
 
 if __name__ == "__main__":
-    update_or_create_downloads_csv_file()
+
+    dict = get_or_create_downloads_dict_from_file()
