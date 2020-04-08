56620d0b8405af604fa006a17c7d5648229226cd
Samuel Jimenez <sjimenezre@gmail.com>
2020-04-08 16:12:44 +0200

    Comment code

--- data_collector/bigquery_pypi/bigquery_pypi.py
+++ data_collector/bigquery_pypi/bigquery_pypi.py
@@ -3,10 +3,24 @@ import pickle
 from datetime import datetime, timedelta
 from google.cloud import bigquery
 
-date = datetime.now().replace(day=1) - timedelta(days=1)
-table_prefix_date = date.strftime("%Y%m")
-downloads_file = "data_collector/bigquery_pypi/data/downloads_{}.pkl".format(table_prefix_date)
 
+"""
+Package downloads data from PyPI is available in a public GCP Bigquery
+database. This module gets the last month downloads data from GCP
+and stores the results in a binary file for further use by the collector.
+"""
+
+# Define last month slug for Bigquery requests and file naming
+last_month = datetime.now().replace(day=1) - timedelta(days=1)
+table_prefix_date = last_month.strftime("%Y%m")
+downloads_file_path = "data_collector/bigquery_pypi/data/downloads_{}.pkl".format(table_prefix_date)
+
+
+"""
+Request Bigquery for last months downloads of every package having more than
+50 downloads. Return the result in a dictionary containing every package name
+as key, and last month downloads as value
+"""
 
 def get_most_downloaded_pkgs(limit=200000, offset=None):
 
@@ -30,21 +44,28 @@ def get_most_downloaded_pkgs(limit=200000, offset=None):
     return downloads_dict
 
 
+"""
+If there is no file containing last months downloads, this method requests
+GCP Bigquery for data and creates the file. Otherwise, it reads the file.
+It returns a dictionary containing every package name as key, and number of
+downloads as value
+"""
+
 def downloads_dict_from_file():
 
-    if os.path.isfile(downloads_file):
-        f = open(downloads_file)
-        print("Downloads file found: {}".format(downloads_file))
+    if os.path.isfile(downloads_file_path):
+        f = open(downloads_file_path)
+        print("Downloads file found: {}".format(downloads_file_path))
     else:
         print("Downloads file NOT found. Requesting downloads from Bigquery")
         downloads_dict = get_most_downloaded_pkgs()
         try:
-            with open(downloads_file, 'wb') as f:
+            with open(downloads_file_path, 'wb') as f:
                 pickle.dump(downloads_dict, f, pickle.HIGHEST_PROTOCOL)
-                print("Downloads file created: {}".format(downloads_file))
+                print("Downloads file created: {}".format(downloads_file_path))
         except IOError as e:
             print("I/O ERROR: {}".format(e))
-    with open(downloads_file, 'rb') as f:
+    with open(downloads_file_path, 'rb') as f:
         dict = pickle.load(f)
         print("Found downloads data for {} packages".format(len(dict)))
         return dict
@@ -52,4 +73,5 @@ def downloads_dict_from_file():
 
 if __name__ == "__main__":
 
-    dict = get_or_create_downloads_dict_from_file()
+    dict = downloads_dict_from_file()
+    print("Downloads data stored for {} packages".format(len(dict)))
